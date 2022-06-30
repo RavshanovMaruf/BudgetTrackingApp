9d5d25e69a8317ab080486e123573e5c44f851e5
RavshanovMaruf <freeimmortal6@gmail.com>
2022-06-30 13:19:43 +0500

    Unnecessary usings removed

--- MicrosLoginTest/Forms/DashboardForm.cs
+++ MicrosLoginTest/Forms/DashboardForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MicrosLoginTest.Forms;        //получить доступ к формам
 using MySql.Data.MySqlClient;       //получить доступ к MYSQL
